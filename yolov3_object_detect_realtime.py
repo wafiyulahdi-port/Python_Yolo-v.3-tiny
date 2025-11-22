8aefd84528a16f24feebdc5bc0bf805e50b496a7
wafiyulahdi-port <wafiyulahdi86@gmail.com>
2025-11-22 13:48:38 +0700

    Update and rename yolov3_object_detec_realtime.py to yolov3_object_detect_realtime.py

--- yolov3_object_detect_realtime.py
+++ yolov3_object_detect_realtime.py
@@ -3,9 +3,9 @@ import numpy as np
 import time
 
 # Load Yolo
-net = cv2.dnn.readNet("yolov3/yolov3-tiny.weights", "yolov3/yolov3-tiny.cfg")
+net = cv2.dnn.readNet("material/yolov3-tiny.weights", "material/yolov3-tiny.cfg")
 classes = []
-with open("yolov3/coco.names", "r") as f:
+with open("material/coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
