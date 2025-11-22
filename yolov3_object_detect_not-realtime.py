19d740d9b33628c1f47a712f21982bc8c94e072d
wafiyulahdi-port <wafiyulahdi86@gmail.com>
2025-11-22 13:50:03 +0700

    Update yolov3_object_detect_not-realtime.py

--- yolov3_object_detect_not-realtime.py
+++ yolov3_object_detect_not-realtime.py
@@ -2,16 +2,16 @@ import cv2
 import numpy as np
 
 # Load Yolo
-net = cv2.dnn.readNet("yolov3/yolov3.weights", "yolov3/yolov3.cfg")
+net = cv2.dnn.readNet("material/yolov3-tiny.weights", "material/yolov3-tiny.cfg")
 classes = []
-with open("coco.names", "r") as f:
+with open("material/coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 # Load image
-img = cv2.imread("img/5.jpg")
+img = cv2.imread("material/bicycle.jpg")
 img = cv2.resize(img, None, fx=0.4, fy=0.4)
 height, width, channels = img.shape
 
